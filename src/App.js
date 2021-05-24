7207bcb4e81cc4076046ca125d1b474ce7be8f60
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-25 00:01:48 +0300

    pulse check 6-8

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import React,{useState} from 'react';
 // jsx
 const App = () => {
   const [posts, setPosts] = useState([
-    {userId:5, Id:130, title: "how to react", body:"it's a long story"},
-    {userId:8, Id:200, title: "next.js", body:"learn react first"} 
+    {userId:5, id:130, title: "how to react", body:"it's a long story"},
+    {userId:8, id:200, title: "next.js", body:"learn react first"} 
   ])
+  const [id,setId] = useState(``);
+  const [userId,setUserId] = useState(``);
+  const [title, setTitle] = useState (``);
+  const [body, setBody] = useState (``);
+
   const PostsRender = posts.map((elem,i)=>{
     return <div key = {i}>
       <div >{elem.title}</div>
@@ -17,6 +22,35 @@ const App = () => {
       <div>
         <h1>Blog App</h1>
         <div>{PostsRender}</div>
+        <button onClick={()=>{
+          const newPost = [{
+            userId:userId,
+            id:id,
+            title:title,
+            body:body
+          }];
+          setPosts([...posts,  {
+            userId:userId,
+            id:id,
+            title:title,
+            body:body
+          }])
+        }}>click me</button>
+        <input type= "number" placeholder="userId" onChange={(e)=>{
+          setUserId(e.target.value);
+          console.log(userId)
+        }}/>
+        <input type= "number" placeholder="id" onChange={(e)=>{
+          setId(e.target.value);
+          console.log(id)
+        }}/>
+        <input type= "string" placeholder="title" onChange={(e)=>{
+          setTitle(e.target.value);
+          
+        }}/>
+        <input type= "string" placeholder="body" onChange={(e)=>{
+          setBody(e.target.value);
+        }}/>
       </div>
   )
 };
